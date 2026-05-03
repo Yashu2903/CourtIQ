21440125877b17f8d27980eba8d31cc3e3eb4300
Yashwanth Kasanneni <yashwanth292003@gmail.com>
2026-05-03 12:21:33 -0400

    Implement replay manifest validation and transformation to point_event_schema

--- infra/local/kafka_setup.sh
+++ infra/local/kafka_setup.sh
@@ -25,6 +25,24 @@ docker exec tennis-kafka kafka-topics.sh \
   --bootstrap-server "$BROKER" \
   --create \
   --if-not-exists \
+  --topic tennis.replay.manifest \
+  --partitions 20 \
+  --replication-factor 1 \
+  --config retention.ms=86400000
+
+docker exec tennis-kafka kafka-topics.sh \
+  --bootstrap-server "$BROKER" \
+  --create \
+  --if-not-exists \
+  --topic tennis.replay.manifest.dlq \
+  --partitions 3 \
+  --replication-factor 1 \
+  --config retention.ms=86400000
+
+docker exec tennis-kafka kafka-topics.sh \
+  --bootstrap-server "$BROKER" \
+  --create \
+  --if-not-exists \
   --topic tennis.replay.audit \
   --partitions 3 \
   --replication-factor 1 \
